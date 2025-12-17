b23ff062d9c8fc63bbe3aef0194a495252b83844
rs4777 <rs4777@columbia.edu>
2025-12-17 13:13:48 -0500

    changed DB

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DB_USER = os.getenv("DB_USER", "myapp_user")
 DB_PASS = os.getenv("DB_PASS", "Password_123")
 DB_NAME = os.getenv("DB_NAME", "addresses")
-DB_HOST = os.getenv("DB_HOST", "34.171.164.80")
+DB_HOST = os.getenv("DB_HOST", "34.10.129.69") #34.10.129.69   34.171.164.80
 DB_PORT = os.getenv("DB_PORT", "3306")
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 print(f"Using local connection via {DB_HOST}:{DB_PORT}")
